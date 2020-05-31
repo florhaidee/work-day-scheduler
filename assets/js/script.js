166d1d23e0f88033439f30f0005fcf9c70206f23
florhaidee <63246286+florhaidee@users.noreply.github.com>
2020-05-31 16:32:25 -0600

    icon for save button changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,54 @@
+var eventsArray = [
+    {
+        id : 9,
+        text: "",
+    },
+    {
+        id : 10,
+        text: "",
+    },
+    {
+        id : 11,
+        text: "",
+    },
+    {
+        id : 12,
+        text: "",
+    },
+    {
+        id : 13,
+        text: "",
+    },
+    {
+        id : 14,
+        text: "",
+    },
+    {
+        id : 15,
+        text: "",
+    },
+    {
+        id : 16,
+        text: "",
+    },
+    {
+        id : 17,
+        text: "",
+    },
+
+];
+var today = moment();
+console.log(today);
 //set currentday
-var currentday = function(){
+var currentDay = function(){
     var day = moment().format("dddd, MMMM Do");
     $("#currentDay").text(day);
 }
-currentday();
+//get event
+$(".time-block").on("change", ".description", function (){
+    var descriptiom = $(this)
+    .val()
+    .trim();
+    console.log(description);
+});
+currentDay();
