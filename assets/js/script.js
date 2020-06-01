d778cc3e3514ce2243b90a035197083f2346aba7
florhaidee <63246286+florhaidee@users.noreply.github.com>
2020-06-01 10:45:01 +0400

    added color-coded feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,74 @@
+//array of objects from all events saved in localStorage
 var events = JSON.parse(localStorage.getItem('events')) || [];
-var today = moment();
 //set currentday
 var currentDay = function(){
     var day = moment().format("dddd, MMMM Do");
     $("#currentDay").text(day);
 }
+//check the status of every event if is past, present or future
+var colorCoded = function(){
+    var hour = moment().hour();
+    console.log(hour);
+    for(var i=9; i<=17; i++){
+        if(i<=12){
+            if(moment(i).isAfter(hour)){
+                 $("#time-"+i).addClass("future");
+            }else if(moment(i).isBefore(hour)){
+                 $("#time-"+i).addClass("past"); 
+            }else {
+                $("#time-"+i).addClass("present");
+            }
+        }else if(i>12) {
+            if(i===13){
+                if(moment(i).isAfter(hour)){
+                     $("#time-1").addClass("future");
+                }else if(moment(i).isBefore(hour)){
+                     $("#time-1").addClass("past"); 
+                }else {
+                    $("#time-1").addClass("present");
+                }
+            } else if(i===14){
+                if(moment(i).isAfter(hour)){
+                     $("#time-2").addClass("future");
+                }else if(moment(i).isBefore(hour)){
+                     $("#time-2").addClass("past"); 
+                }else {
+                    $("#time-2").addClass("present");
+                }
+            }else if(i===15){
+                if(moment(i).isAfter(hour)){
+                     $("#time-3").addClass("future");
+                }else if(moment(i).isBefore(hour)){
+                     $("#time-3").addClass("past"); 
+                }else {
+                    $("#time-3").addClass("present");
+                }
+            }else if(i===16){
+                if(moment(i).isAfter(hour)){
+                     $("#time-4").addClass("future");
+                }else if(moment(i).isBefore(hour)){
+                     $("#time-4").addClass("past"); 
+                }else {
+                    $("#time-4").addClass("present");
+                }
+            }else if(i===17){
+                if(moment(i).isAfter(hour)){
+                     $("#time-5").addClass("future");
+                }else if(moment(i).isBefore(hour)){
+                     $("#time-5").addClass("past"); 
+                }else {
+                    $("#time-5").addClass("present");
+                }
+            }
+        }
+    }
+}
+//function to load on page all events saved in localStorage
 var loadEvents = function(i, event){
     var id = event.hour.slice(0,2);
     $("#time-"+id).text(event.text);
 }
+//function to search for every event saved in localStorage
 var refreshPage = function() {
     // loop over object properties
     $.each(events, function (i, event) {
@@ -29,3 +89,6 @@ $(".btn").on("click", function(event){
 });
 currentDay();
 refreshPage();
+colorCoded();
+//check every half hour the color-coded function
+setInterval(function () {colorCoded() }, (1000 * 60) * 30);
