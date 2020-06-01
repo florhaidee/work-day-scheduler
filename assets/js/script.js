75b5f910a4d6904d4ca3974fd87d54d3dabd0008
florhaidee <63246286+florhaidee@users.noreply.github.com>
2020-05-31 20:52:46 -0600

    saved events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,24 @@
-var eventsArray = [
-    {
-        id : 9,
-        text: "",
-    },
-    {
-        id : 10,
-        text: "",
-    },
-    {
-        id : 11,
-        text: "",
-    },
-    {
-        id : 12,
-        text: "",
-    },
-    {
-        id : 13,
-        text: "",
-    },
-    {
-        id : 14,
-        text: "",
-    },
-    {
-        id : 15,
-        text: "",
-    },
-    {
-        id : 16,
-        text: "",
-    },
-    {
-        id : 17,
-        text: "",
-    },
+var eventsArray = JSON.parse(localStorage.getItem('event')) || [];
 
-];
 var today = moment();
-console.log(today);
 //set currentday
 var currentDay = function(){
     var day = moment().format("dddd, MMMM Do");
     $("#currentDay").text(day);
 }
-//get time event
-$(".time-block").on("click", function (){
-    var hour = $(this)
-    .text()
-    .trim();
-    console.dir(event.target);
-    console.log(hour);
-});
-// textarea was changed
-$(".form-control").on("change", function(){
-    var text = $(this)
-    .val();
-    console.log(text);
+var saveEvent = function(eventOb) {
+    console.log(eventOb);
+} 
+//when a save button is clicked
+$(".btn").on("click", function(event){
+    event.preventDefault();
+    eventOb = {
+        text: $(this).prev().val().trim(),
+        hour: $(this).closest(".time-block").text().trim()
+    }
+    eventsArray.push(eventOb)
+    console.log(eventsArray)
+    saveEvent(eventOb);
+    localStorage.setItem('event', JSON.stringify(eventsArray))
 });
 currentDay();
